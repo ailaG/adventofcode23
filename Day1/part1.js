0ef9a332e616543f51dd01b84e6a8d51fd952ecf
Galia <gitlab@galiaba.com>
2023-12-01 21:49:40 +0200

    Day 1-1 done and nice

--- Day1/part1.js
+++ Day1/part1.js
@@ -2,13 +2,12 @@
 
 import { getInput } from './helpers.mjs';
 
-const input = getInput('./input-example.txt').split('\n');
+const input = getInput('./input1.txt').split('\n');
 
 console.log('Input:',input);
 
-
-const numbers = input.map(line => line.match(/[\d]/g).join(''))
-const sums = numbers.map(line=> Number(String(line.at(0)) + String(line.at(-1))) );
-const res = sums.reduce((sofar,curr) => sofar+curr);
-
-console.log(res);
+console.log (
+	input	.map(line => line.match(/[\d]/g).join(''))
+			.map(line=> Number(String(line.at(0)) + String(line.at(-1))) )
+			.reduce((sofar,curr) => sofar+curr)
+);
